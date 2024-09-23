dcdaf2533fc67d36435119df923d8d31b07e5607
cbravop <cbravop309@gmail.com>
2024-09-23 15:03:11 -0500

    agrego random y max

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ const num2 = 20
 const suma = num1 + num2
 
 console.log("El resultado es", suma)
+
+console.log("Un numero random", Math.random())
+
+const numeros = [4, 7, 1]
+console.log("El maximo es", Math.max(...numeros))
